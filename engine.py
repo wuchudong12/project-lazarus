c80af0ee613dc9a5a435dd8175cdd709bc415606
vhsvhs <victorhansonsmith@gmail.com>
2014-02-27 14:03:13 -0800

    I fixed a critical bug causing the print statement 'cp: XX : No such file or directory' See Notes.

--- engine.py
+++ engine.py
@@ -177,7 +177,7 @@ class Engine:
                 #os.system("ls -alh " + outputDirectory)
                 #os.system("ls -alh " + thisExecutionDir)
                 #print "\n\n engine 173 \n\n"
-                print "cp " + outputDirectory + "/reformatted_alignment.phy " + thisExecutionDir + "/reformatted_alignment.phy"
+                os.system("cp " + outputDirectory + "/reformatted_alignment.phy " + thisExecutionDir + "/reformatted_alignment.phy")
                 #os.system("cp " + outputDirectory + "/reformatted_alignment.phy " + thisExecutionDir + "/")#reformatted_alignment.phy")
                 #os.system("ln -s " + outputDirectory + "/reformatted_alignment.phy " + thisExecutionDir + "/reformatted_alignment.phy")
                 
